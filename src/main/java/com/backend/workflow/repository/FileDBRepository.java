3b5eccc81cf470c871aa5722a49302bd9c7d5dc1
ismael <ismael.vargas@fundacred.org.br>
2021-10-05 20:45:59 -0300

    pagamentos e uploads funcionando

---
+++ src/main/java/com/backend/workflow/repository/FileDBRepository.java
@@ -0,0 +1,11 @@
+package com.backend.workflow.repository;
+
+import com.backend.workflow.entity.FileDB;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface FileDBRepository extends JpaRepository<FileDB, String> {
+
+}
---
+++ src/main/java/com/backend/workflow/service/FileStorageService.java
@@ -0,0 +1,34 @@
+package com.backend.workflow.service;
+
+import com.backend.workflow.entity.FileDB;
+import com.backend.workflow.repository.FileDBRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+
+@Service
+public class FileStorageService {
+
+    @Autowired
+    private FileDBRepository fileDBRepository;
+
+    public FileDB store(MultipartFile file, Integer idSolicitacao) throws IOException {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        FileDB FileDB = new FileDB(fileName, file.getContentType(), file.getBytes(), idSolicitacao);
+
+        return fileDBRepository.save(FileDB);
+    }
+
+    public FileDB getFile(String id) {
+        return fileDBRepository.findById(id).get();
+    }
+
+    public Stream<FileDB> getAllFiles() {
+        return fileDBRepository.findAll().stream();
+    }
+}
