401b76894e0a48680041e5de87291a41acbed95e
ismael <ismael.vargas@fundacred.org.br>
2021-10-30 04:07:02 -0300

    cosulta finalizada

--- src/main/java/com/backend/workflow/repository/PagamentoRepository.java
+++ src/main/java/com/backend/workflow/repository/PagamentoRepository.java
@@ -18,10 +18,10 @@ public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
 //    List<Pagamento> findByNomeFornecedorAndNomePedido(
 //            @Param("nomePedido") String nomePedido, @Param("nomeFornecedor") String nomeFornecedor, @Param("nomeStatus") String nomeStatus);
 
-    @Query( "select s from Pagamento s join s.tipoStatus t " +
-            "where upper( t.nomeStatus ) like upper( :nomeStatus ) and upper( s.nomeFornecedor ) like upper( :nomeFornecedor )" )
-    List<Pagamento> findByNomeFornecedorAndNomePedido(
-            @Param("nomeStatus") String nomeStatus,
-            @Param("nomeFornecedor") String nomeFornecedor);
+    @Query( "select s from Pagamento s join fetch s.tipoStatus t " +
+            "where upper( s.nomeFornecedor ) like upper( :nomeFornecedor ) and upper( t.nomeStatus ) like upper( :nomeStatus )" )
+    List<Pagamento> findByNomeFornecedorAndNomeStatus(
+            @Param("nomeFornecedor") String nomeFornecedor,
+            @Param("nomeStatus") String nomeStatus);
 
 }
