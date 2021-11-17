0adab57dab95f419b0751762a74a709349e71d1f
ismael <ismael.vargas@fundacred.org.br>
2021-11-16 23:17:11 -0300

    buscar documentos implementado e send emails iniciado

--- src/main/java/com/backend/workflow/service/FileStorageService.java
+++ src/main/java/com/backend/workflow/service/FileStorageService.java
@@ -31,4 +31,8 @@ public class FileStorageService {
     public Stream<FileDB> getAllFiles() {
         return fileDBRepository.findAll().stream();
     }
+
+    public Stream<FileDB> getDocumentos(Integer idPagamento) {
+        return fileDBRepository.findByIdPagamento(idPagamento).stream();
+    }
 }
