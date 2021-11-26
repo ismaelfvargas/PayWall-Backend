45de6ed0cfd147faf0461738e7a49e1bd12473e4
ismael <ismael.vargas@fundacred.org.br>
2021-11-26 02:09:20 -0300

    documentos e exclusão finalizados

---
+++ src/main/java/com/backend/workflow/entity/EmailRequest.java
@@ -0,0 +1,42 @@
+package com.backend.workflow.entity;
+
+
+public class EmailRequest {
+
+    private String to;
+    private String subject;
+    private String message;
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "EmailRequest{" +
+                "to='" + to + '\'' +
+                ", subject='" + subject + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
